c7470c7bf90f424cdc9d2b19c743d4a17fe61bca
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-19 21:36:31 -0500

    added seed data

---
+++ Develop/db/seeds.sql
@@ -0,0 +1,36 @@
+-- Insert data into the departments table
+INSERT INTO department (name)
+    VALUES ('Front-End Development');
+
+INSERT INTO department (name)
+    VALUES ('Back-End Development');
+
+INSERT INTO department (name)
+    VALUES ('Management');
+
+-- Insert data into the role table
+INSERT INTO role (title, salary, department_id)
+    VALUES ("Front-End Team Lead", 120000, 1);
+
+INSERT INTO role (title, salary, department_id)
+    VALUES ("Front-End Developer", 100000, 1);
+
+INSERT INTO role (title, salary, department_id)
+    VALUES ("Back-End Team Lead", 130000, 2);
+
+INSERT INTO role (title, salary, department_id)
+    VALUES ("Back-End Developer", 100000, 2);
+
+INSERT INTO role (title, salary, department_id)
+    VALUES ("CTO", 250000, 3);
+
+INSERT INTO role (title, salary, department_id)
+    VALUES ("CEO", 750000, 3);
+
+-- Insert data into the employee table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES ("Tony", "Stark", )
+
+Questions for tutor:
+How does inputting data work in joined fields?
+Help writing queries to do things when option is chosen from inquirer
