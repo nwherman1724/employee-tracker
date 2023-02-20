3e208d64aaa56a1d94d781eaab81f4a4e7e0c7e2
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-19 20:58:23 -0500

    joined tables

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -6,14 +6,18 @@ USE business_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT,
-    name VARCHAR(30)
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
     id INT AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+
 );
 
 CREATE TABLE employee(
@@ -21,5 +25,8 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
