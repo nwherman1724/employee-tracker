d9e1f95b5d7d9264eb0397ea0d498993072fc2a3
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-20 20:22:33 -0500

    bug fix

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,7 @@ const mysql = require('mysql2');
         const connection = mysql.createConnection({
         host: '127.0.0.1',
         user: 'root',
-        password: 'Danalee524',
+        password: '',
         database: 'business_db'
         });
 
