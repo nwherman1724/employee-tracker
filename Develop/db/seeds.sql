b1e91776e8c30897b8f1ec51f356af221bcf81b8
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-24 06:37:35 -0500

    edit file structure

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -33,7 +33,3 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES ("Pepper", "Potts", 5, 1);
-
--- Questions for tutor:
--- How does inputting data work in joined fields? x
--- Help writing queries to do things when option is chosen from inquirer
--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
         const connection = mysql.createConnection({
         host: '127.0.0.1',
         user: 'root',
-        password: '',
+        password: 'Danalee524',
         database: 'business_db'
         });
 
